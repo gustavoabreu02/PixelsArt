b89157a3326d8f659d711e7a787536b6007908ac
Gustavo Abreu <gustavofart371@gmail.com>
2022-03-21 19:20:30 -0300

    requisito 5

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//requisito 1
+//requisito 2
 let acessoMain = document.querySelector('main');
 function coresPaleta () {
     let section = document.createElement('section');
@@ -10,8 +10,8 @@ function coresPaleta () {
         let divColor = document.createElement('div');
         divColor.className = 'color';
         divColor.style.border = '1px solid black';
-        divColor.style.height = '110px';
-        divColor.style.width = '110px';
+        divColor.style.height = '50px';
+        divColor.style.width = '50px';
         divColor.style.display = 'inline-block';
         divColor.style.margin = '5px'
         divColor.style.backgroundColor = cores[i];
@@ -19,3 +19,24 @@ function coresPaleta () {
     }
 } 
 coresPaleta(); 
+
+//requisito 4
+function pixels () {
+    let section = document.createElement('section');
+    section.id = 'pixel-board';
+    acessoMain.appendChild(section);
+    let acessoSection = document.querySelector('#pixel-board')
+    for (let i = 0; i < 25; i += 1) {
+        let divPixels = document.createElement('div');
+        divPixels.className = 'pixel';
+        divPixels.style.backgroundColor = 'white';
+        divPixels.style.border = '1px solid black';
+        divPixels.style.height = '40px';
+        divPixels.style.width = '40px';
+        acessoSection.appendChild(divPixels)
+    }
+    section.style.display = 'grid'
+    section.style.gridTemplateColumns = '40px 40px 40px 40px 40px'
+    section.style.margin = '5px'
+}
+pixels ();
